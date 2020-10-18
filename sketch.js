9b357ab734c634763601173acf43906f65509807
maxyxy <68803017+maxyxy@users.noreply.github.com>
2020-10-18 14:59:18 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,45 +1,33 @@
-var canvas, backgroundImage;
+var hours 
+var minutes
+var seconds
 
-var gameState = 0;
-var playerCount;
-var allPlayers;
-var distance = 0;
-var database;
+hr=hours();
+mn = minutes();
+sc = second ();
 
-var form, player, game;
+anglemode(degrees);
+hrAngle=map(hr,0,12,0,24);
+mnAngle=map(mn,0,1,0,60);
+scAngle=Map(sc,0,60,0,360);
 
-var cars, car1, car2, car3, car4;
-
-var track, car1_img, car2_img, car3_img, car4_img; 
-function preload(){ track = loadImage("../images/track.jpg"); 
-car1_img = loadImage("../images/car1.png");
-car2_img = loadImage("../images/car2.png");
- car3_img = loadImage("../images/car3.png");
-  car4_img = loadImage("../images/car4.png");
-   ground = loadImage("../images/ground.png"); }
-
-function setup(){
-  canvas = createCanvas(displayWidth - 20, displayHeight-30);
-  database = firebase.database();
-  game = new Game();
-  game.getState();
-  game.start();
-
+function setup() {
+  createCanvas(720, 400);
+  noStroke();
+  
 }
 
-
-
-function draw(){
-  if(playerCount === 4){
-    game.update(1);
-  }
-  if(gameState === 1){
-    clear();
-    game.play();
-   
-  }
-  if( gameState===2){
-    game.end();
-  }
-
+function draw() {
+  background(0);
+  // Scale the mouseX value from 0 to 720 to a range between 0 and 175
+  let c = map(mouseX, 0, width, 0, 175);
+  // Scale the mouseX value from 0 to 720 to a range between 40 and 300
+  let d = map(mouseX, 0, width, 40, 300);
+  fill(255, c, 0);
+  ellipse(width/2, height/2, d, d);
 }
+arc(50, 55, 50, 50, 0, HALF_PI);
+noFill();
+arc(50, 55, 60, 60, HALF_PI, PI);
+arc(50, 55, 70, 70, PI, PI + QUARTER_PI);
+arc(50, 55, 80, 80, PI + QUARTER_PI, TWO_PI);
